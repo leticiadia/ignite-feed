6065f5e3622627925c97c412b08d25993aed8ad6
Leticia Dias <contatoleticiadia@gmail.com>
2024-05-06 14:38:07 -0300

    feat(components): created handleDeleteComment() function

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-export function Comment() {
+export function Comment({ content, OnDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="src/assets/mikkaiser.jpeg" />
@@ -21,12 +25,12 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Leticia, parabéns!</p>
+          <p>{content}</p>
         </div>
 
         <footer>
